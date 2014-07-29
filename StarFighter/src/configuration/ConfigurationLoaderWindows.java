2f03dd42e0fa2a51a32f3907f48bee35180a171a
nathan <nathan@nathan-laptop>
2014-07-29 17:57:07 +0200

    adding default config initializer

--- StarFighter/src/configuration/ConfigurationLoaderWindows.java
+++ StarFighter/src/configuration/ConfigurationLoaderWindows.java
@@ -1,10 +1,20 @@
 package configuration;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,7 +25,7 @@ public class ConfigurationLoaderWindows implements Configuration{
     private String locationFolder;
     private final String FOLDERNAME = "starfighter";
     private final String FILENAME = "starfighterconfig";
-    private final String DEFAULTFILE = "assets/config/defaultConfig.conf";
+    private final String DEFAULTFILE = "/assets/config/defaultConfig.conf";
     
     public ConfigurationLoaderWindows(){
         locationFolder = System.getenv("APPDATA");
@@ -33,14 +43,28 @@ public class ConfigurationLoaderWindows implements Configuration{
     private void loadConfigFile(){
         //controleren of er een configfile aanwezig is
         Path configFilePath = Paths.get(locationFolder + "/" + FOLDERNAME + "/" + FILENAME);
-        if(Files.exists(configFilePath)){
-            //bestaand bestand inlezen
-            configFile = new File(configFilePath.toString());
-        }else {
+        if(!Files.exists(configFilePath)){
             //default bestand kopieren en inlezen
-            InputStream is = this.getClass().getResourceAsStream(DEFAULTFILE);
-
+            try{
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(DEFAULTFILE)));
+                FileWriter writer = new FileWriter(Paths.get(locationFolder + "/" + FOLDERNAME + "/" + FILENAME).toString());
+                BufferedWriter out = new BufferedWriter(writer);
+                String line = inputReader.readLine();
+                while (line != null){
+                    line = line + "\n";
+                    out.write(line);
+                    line = inputReader.readLine();
+                }
+                inputReader.close();
+                out.close();
+                //JOptionPane.showMessageDialog(null, inputReader.lines().count());
+            }catch(IOException ioe){
+                System.err.println("inputexception");
+            }
         }
+        
+        //bestaand bestand inlezen
+        configFile = new File(configFilePath.toString());
     }
     
     @Override
